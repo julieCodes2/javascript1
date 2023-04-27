5b470656848bec2aed1715bbfec92e1a8ca229c7
julieCodes2 <julieann.zeiger@gmail,com>
2023-04-27 10:23:44 +0200

    Lesson 1 Assignment

---
+++ JS_Lesson 1/Assignment JS_Lesson1/assignment1.js
@@ -0,0 +1,9 @@
+// alert('Hello You!');
+
+function myFunction() {
+  let person = prompt('Please enter your name', 'Harry Potter');
+  if (person != null) {
+    document.getElementById('demo').innerHTML =
+      'Hello ' + person + '! How are you today?';
+  }
+}
