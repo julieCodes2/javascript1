d49eed026d37d46f38b7018c5a02e5c67c0a64e3
julieCodes2 <julieann.zeiger@gmail.com>
2023-05-03 15:39:12 +0200

    JS Assignment3

---
+++ Session3/JS_Assignment3/Assignment3.js
@@ -0,0 +1,10 @@
+function addAttendee() {
+let name = attendeeList.first.value + " " 
+     + attendeeList.last.value;
+     //alert(attendeeList.first.value)
+     //alert(name);
+    let ul = document.getElementById("attendees");
+    let li = document.createElement("LI");
+    li. appendChild(document.createTextNode(name));
+    ul.appendChild(li);
+}
