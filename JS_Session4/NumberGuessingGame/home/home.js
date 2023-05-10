5a8d7855d2df0419040308dd805d4dffd2322288
julieCodes2 <julieann.zeiger@gmail.com>
2023-05-10 02:42:02 +0200

    Codes are working now

--- JS_Session4/NumberGuessingGame/home/home.js
+++ JS_Session4/NumberGuessingGame/home/home.js
@@ -1,94 +1,74 @@
-//-----------------------//
-        // This function will retrieve the form data, and check for validation errors (they need to submit valid numbers AND the low number has to be less than the high number). If there is an error, display the error by calling the updateDOM function and stop the remaining code execution. If there is no error, call the numberGuessGame and pass in min and max as parameters.
+function submitData() {
         // retrieve form data -- min and max
-//if error
-  //updateDOM("ERROR", "red");
-// else
-//numberGuessGame(min, max)
- // handle submit
-  function submitData() {
- let min =
- parseInt(document.getElementById('min').value);
- let max=
- parse(document.getElementById('max').value);
+        let min = Number(gameform.min.value);
+        let max = Number(gameform.max.value);
+        //console.log(min);
+        //console.log(max);
+        //console.log(typeof min);
 
- if (!Number.isInteger(min) || ! Number.isInteger(max)) {
-    updateDOM('specified low and high numbers must be integer numbers', 'red');
-    return 
- }
- if (min >= max) {
-    updateDOM ('low number must be smaller than high number', 'red');
-    return
- }
- //numberGuessGame(min,max);
- }
-//------------------//
-        // This function will display the error message if there is a validation error. And later it will also display "CONGRATULATIONS!!! __ is correct!". The value parameter will either be the error message string, or the congratulations message. The color you can use to make the error red, or the congrats message a special color.
-        // document.getElementById("output").innerHTML = value; // make sure you have a div in html file with id of output.
-        // update color of div as well
-        //document.getElementById("output").innerHTML = value;
-         function updateDOM(value, color) {
-    const div =
-    document.getElementById("output");
-    div.style.color = color;
-    div.innerHTML=value;
- }
- 
- 
-//-----------------------//
-         // This function will include the loop and the logic to drive the game. Use a do while loop with a condition similar to while(userGuess != actualNumber). Inside the loop, use prompt() to prompt the user for their next guess, and display their progress letting them know if they need to guess higher or lower in the prompts. This function will take a fair bit of thought. Perhaps this code structure may be helpful as you write this code:
-        //let num = randomNumber(min, max)
-        
-        // declare message variable with something like , "I'm thinking of a number between 'min' and 'max', try to guess it"
-        //alert("Please guess a number between 1 and 10";)
-        //let guess; // declare guess as empty variable so you can modify it from the loop
-    //     do {
-    //         guess = parseInt(prompt(message))
-    //         // if too low modify message to say that their guess is too low
-    //         // if too high, modify message to say that they guess is too high
-    //     } while(..guess is wrong..)
-    //     // modify message to say congratulations!!!
-    //     updateDOM(message, "green");
-     function numberGuessGame(min, max){
+//it will be 5 and 10
+
+if (min == "" || max == ""){
+    let value = "Both fields must be filled in.";
+    let color = red;
+    updateDOM(value, color);
+    return false;
+    //if it fails the check there
+}
+
+if (min >= max) {
+    let value = "the min value cannot be greater than max";
+    let color = "red";
+    updateDOM(value,color)
     
-    const secretNumber = randomNumber(min, max);
-    let message = 'Please guess a number between ' + min + ' and ' + max + '!';
-    let guess;
-    do {
-        const value = prompt(message);
-        if (value===null) {
-          
-            updateDOM('Game was cancelled!', 'blue');
-            
-            return
-        }
-        guess = parseInt(value);
-        //only modify message if a valid number was specified
-
-    if (Number.isInteger(guess)) {
-        if (guess < secretNumber) {
-            message = 'You need to guess higher than ' + guess + ', please pick another number';
-        }else {
-            message = 'You need to guess lower than ' + guess + ', please try again!';
-        }
+}
+numberGuessGame(min,max);
+return false;
+        //if error
+            //updateDOM("ERROR", "red");
+        // else
+           // numberGuessGame(min, max)
     }
-            }while (guess !== secretNumber);
-            
-            //updateDOM('CONGRATULATIONS!!! ' + secretNumber + ' is correct', 'green');
-            document.write('CONGRATULATIONS!!! ' + secretNumber + ' is correct');
-                        // document.write('CONGRATULATIONS!!! ' + secretNumber + ' is correct', 'green');
+
+    function updateDOM(value, color) {
+        document.getElementById("output").innerHTML = value; 
+        document.getElementById("output").style.color = color;
+        // make sure you have a div in html file with id of output.
+        // update color of div as well
+    }
+
+    function numberGuessGame(min, max) {
+        // see logic and code above
+        //console.log ("let the game begin");
+        //console.log("min " + min + "max " + max);
+         
+        let num = Math.ceil(randomNumber(min, max));
+        console.log(num);
+        // declare message variable with something like , "I'm thinking of a number between 'min' and 'max', try to guess it"
+        let message = "I'm thinking of a number between" + min + "and " + max + " try to guess it."
+        //let guess = parseInt(prompt(message));
+        let guess;
+        do {
+            guess = parseInt(prompt(message));
+            console.log(guess);
+        if (guess < num) {
+            message= "Guess is too low. try again!";
+            guess = parseInt(prompt(message));
         }
+        if (guess > num) {
+            message= "Guess is too high. try again!";
+            guess = parseInt(prompt(message));
+        }
+         if (isNan(guess)) {
+           break;
+        }
+        } while(guess !== num);
+    updateDOM('CONGRATULATIONS!!! ' + guess + ' is correct', 'green');
     
-//---------------//
+    }
+    function randomNumber(min, max) { 
+        return Math.random() * (max - min) + min;
+    } 
+    
 
-        // This function will take the numbers the user put in for the range of the game. It will use Math.random to produce a whole,random number within that range. This reference may be helpful.
-        //return Math.floor(Math.random() * (10 - 1) + min);
-            function randomNumber(min, max) { 
-//return Math.round(Math.random() * (min - max) + min);
-return Math.floor(Math.random() * (5 - 1) + min);
-	}
-   //
-   numberGuessGame(1,5);
-   
-
-
+    
